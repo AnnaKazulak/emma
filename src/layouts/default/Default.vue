bfc3582c87b3ecb083075335b16e3680d35b42ad
Anna Kazulak <anna.kazulak@gmail.com>
2024-05-24 16:04:20 +0200

    refactor: redesign navbar and integrate logo

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -8,16 +8,11 @@ const drawer = ref(false);
   <v-layout class="rounded rounded-md">
     <v-app-bar>
       <template #prepend>
-        <v-app-bar-nav-icon
-          variant="text"
-          @click.stop="drawer = !drawer"
-        ></v-app-bar-nav-icon>
-        <v-toolbar-title>
-          <router-link to="/" class="text-decoration-none">
-            My Awesome App
-          </router-link>
-        </v-toolbar-title>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       </template>
+      <router-link to="/" class="logo-link">
+        <img src="src/assets/emma-logo-removebg-2.png" alt="Emma logo" class="logo-image" />
+      </router-link>
       <template #append>
         <v-menu>
           <template v-slot:activator="{ props }">
@@ -26,11 +21,7 @@ const drawer = ref(false);
             </v-avatar>
           </template>
           <v-list>
-            <v-list-item
-              v-for="(item, index) in ['Profile', 'Settings', 'Logout']"
-              :key="index"
-              :value="index"
-            >
+            <v-list-item v-for="(item, index) in ['Profile', 'Settings', 'Logout']" :key="index" :value="index">
               <v-list-item-title>{{ item }}</v-list-item-title>
             </v-list-item>
           </v-list>
@@ -40,24 +31,38 @@ const drawer = ref(false);
 
     <v-navigation-drawer expand-on-hover rail v-model="drawer">
       <v-list density="compact" nav>
-        <v-list-item
-          prepend-icon="mdi-account"
-          title="Employees"
-          :to="`/employees`"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-comment-outline"
-          title="Comments"
-          :to="`/comments`"
-        ></v-list-item>
+        <v-list-item prepend-icon="mdi-account" title="Employees" :to="`/employees`"></v-list-item>
+        <v-list-item prepend-icon="mdi-comment-outline" title="Comments" :to="`/comments`"></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-main
-      class="ml-3"
-      style="min-height: 300px"
-    >
+    <v-main class="ml-3" style="min-height: 300px">
       <router-view></router-view>
     </v-main>
   </v-layout>
 </template>
+
+<style scoped>
+.toolbar-logo {
+  flex-grow: 0;
+  /* Prevent the title from taking too much space */
+}
+
+.logo-link {
+  display: flex;
+  /* Ensures the link and image are aligned properly */
+  align-items: center;
+  /* Vertically aligns the logo */
+}
+
+.logo-image {
+  height: 200px;
+  /* Adjust size as needed */
+  width: auto;
+  /* Maintain aspect ratio */
+}
+
+img.logo-image {
+  margin-bottom: 10px;
+}
+</style>
