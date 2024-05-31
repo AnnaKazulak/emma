1e7d015adbda86ac9db4732164ccf0eb9cd3bbd5
Anna Kazulak <anna.kazulak@gmail.com>
2024-05-31 21:41:24 +0200

    Enhance: Add `startDate` and `dateOfBirth` fields, and implement validation for mandatory fields with error messages.

--- src/components/EmployeePublicProfile.vue
+++ src/components/EmployeePublicProfile.vue
@@ -1,10 +1,14 @@
 <template>
     <v-card>
         <v-card-text>
-            <v-text-field v-if="editMode" label="First Name" v-model="editableEmployee.firstName"></v-text-field>
+            <v-text-field v-if="editMode" label="First Name" v-model="editableEmployee.firstName"
+                :rules="[v => !!v || 'First Name is required']" :error-messages="firstNameErrors"
+                @input="validateFirstName"></v-text-field>
             <div v-else>First Name: {{ editableEmployee.firstName }}</div>
 
-            <v-text-field v-if="editMode" label="Last Name" v-model="editableEmployee.lastName"></v-text-field>
+            <v-text-field v-if="editMode" label="Last Name" v-model="editableEmployee.lastName"
+                :rules="[v => !!v || 'Last Name is required']" :error-messages="lastNameErrors"
+                @input="validateLastName"></v-text-field>
             <div v-else>Last Name: {{ editableEmployee.lastName }}</div>
 
             <v-select v-if="editMode" label="Gender" :items="['Male', 'Female', 'Divers']"
@@ -16,7 +20,7 @@
 
             <v-select v-if="editMode" label="Department" :items="[
                 'Engineering', 'HR', 'Marketing',
-                 'Sales', 'Finance', 'Legal', 'IT', 'Other', 'AI', 'UX','Design'
+                'Sales', 'Finance', 'Legal', 'IT', 'Other', 'AI', 'UX', 'Design'
             ]" v-model="editableEmployee.department" chips></v-select>
             <div v-else>Department: {{ editableEmployee.department }}</div>
 
@@ -30,6 +34,15 @@
             ]" v-model="editableEmployee.status" chips></v-select>
             <div v-else>Status: {{ editableEmployee.status }}</div>
 
+            <v-text-field v-if="editMode" label="Start Date" type="date" v-model="editableEmployee.startDate"
+                :rules="[v => !!v || 'Start Date is required']" :error-messages="startDateErrors"
+                @input="validateStartDate"></v-text-field>
+            <div v-else>Start Date: {{ editableEmployee.startDate }}</div>
+
+            <v-text-field v-if="editMode" label="Date of Birth" type="date"
+                v-model="editableEmployee.dateOfBirth"></v-text-field>
+            <div v-else>Date of Birth: {{ editableEmployee.dateOfBirth }}</div>
+
             <div v-if="editMode">Address:
                 <v-text-field label="Street" v-model="editableEmployee.address.street"></v-text-field>
                 <v-text-field label="House Number" v-model="editableEmployee.address.houseNumber"></v-text-field>
@@ -37,10 +50,10 @@
                 <v-text-field label="City" v-model="editableEmployee.address.city"></v-text-field>
             </div>
             <div v-else>Address:
-                <div>Street {{ editableEmployee.address.street }}</div>
-                <div>House Number {{ editableEmployee.address.houseNumber }}</div>
-                <div>Zip Code {{ editableEmployee.address.zipCode }}</div>
-                <div>City {{ editableEmployee.address.city }}</div>
+                <div>Street: {{ editableEmployee.address.street }}</div>
+                <div>House Number: {{ editableEmployee.address.houseNumber }}</div>
+                <div>Zip Code: {{ editableEmployee.address.zipCode }}</div>
+                <div>City: {{ editableEmployee.address.city }}</div>
             </div>
         </v-card-text>
 
@@ -49,9 +62,9 @@
             <v-btn color="error" @click.stop="cancelEdit" v-if="editMode">Cancel</v-btn>
             <v-btn color="primary" @click.stop="toggleEditMode" v-else>Edit Employee</v-btn>
         </v-card-actions>
-
     </v-card>
 </template>
+
 <script lang="ts">
 import { defineComponent, reactive, ref, watch } from 'vue';
 
@@ -70,16 +83,22 @@ export default defineComponent({
             position: '',
             department: '',
             status: '',
+            startDate: '',
+            dateOfBirth: '',
             address: {
                 street: '',
                 houseNumber: '',
                 zipCode: '',
                 city: ''
             },
-            gender: ""
+            gender: ''
         });
 
         const editMode = ref(false);
+
+        const firstNameErrors = ref<string[]>([]);
+        const lastNameErrors = ref<string[]>([]);
+        const startDateErrors = ref<string[]>([]);
 
         watch(() => props.employee, (newVal) => {
             if (newVal) {
@@ -95,10 +114,28 @@ export default defineComponent({
             }
         }
 
+        function validateFirstName() {
+            firstNameErrors.value = editableEmployee.firstName ? [] : ['First Name is required'];
+        }
+
+        function validateLastName() {
+            lastNameErrors.value = editableEmployee.lastName ? [] : ['Last Name is required'];
+        }
+
+        function validateStartDate() {
+            startDateErrors.value = editableEmployee.startDate ? [] : ['Start Date is required'];
+        }
+
         function saveChanges() {
-            console.log("Saving changes for:", editableEmployee);
-            emit('update-employee', { ...editableEmployee });
-            editMode.value = false;
+            validateFirstName();
+            validateLastName();
+            validateStartDate();
+
+            if (!firstNameErrors.value.length && !lastNameErrors.value.length && !startDateErrors.value.length) {
+                console.log("Saving changes for:", editableEmployee);
+                emit('update-employee', { ...editableEmployee });
+                editMode.value = false;
+            }
         }
 
         function cancelEdit() {
@@ -113,7 +150,13 @@ export default defineComponent({
             editMode,
             toggleEditMode,
             saveChanges,
-            cancelEdit
+            cancelEdit,
+            firstNameErrors,
+            lastNameErrors,
+            startDateErrors,
+            validateFirstName,
+            validateLastName,
+            validateStartDate
         };
     }
 });
